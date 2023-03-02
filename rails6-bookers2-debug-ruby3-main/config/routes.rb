e134447b607a7d95ee2089b3354bb02e124b6ba8
EC2 Default User <ec2-user@ip-172-31-13-47.ap-northeast-1.compute.internal>
2023-03-02 03:30:53 +0000

    [Update]いいね機能途中

--- rails6-bookers2-debug-ruby3-main/config/routes.rb
+++ rails6-bookers2-debug-ruby3-main/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :books, only: [:index,:show,:edit,:new,:create,:destroy,:update]
+  resources :books, only: [:index,:show,:edit,:new,:create,:destroy,:update] do
+    resource :favorites, only: [:create, :destroy]
+  end
+
   resources :users, only: [:index,:show,:edit,:update]
 
   get "home/about"=>"homes#about"
